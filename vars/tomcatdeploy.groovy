20862085d68eefb95fae0dc4c617c9d3fffccd34
dhanuar <95330710+dhanuar@users.noreply.github.com>
2022-01-11 21:45:40 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -3,7 +3,7 @@ def call(credId,tomcatIp,tomcatUser){
                 // copy a file
                 sh"""
                 scp -o StrictHostKeyChecking=no target/myweb*.war ${tomcatUser}@${tomcatIp}:/opt/tomcat8/webapps/myweb.war
-               ssh ${tomcatUser}@${tomcatIp} /opt/tomcat8/bin/shutdown.sh
+                ssh ${tomcatUser}@${tomcatIp} /opt/tomcat8/bin/shutdown.sh
                 ssh ${tomcatUser}@${tomcatIp} /opt/tomcat8/bin/startup.sh
                 """
   }
